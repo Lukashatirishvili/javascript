844dc5c8ef18d31419fd3b7f98c4c6051604a7b2
Lukashatirishvili <126322793+Lukashatirishvili@users.noreply.github.com>
2023-06-05 12:37:58 +0400

    Add files via upload
    https://javascript.info/alert-prompt-confirm

---
+++ JavaScript.info/2.6 - alert - prompt - confirm .js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Alert
+
+alert("Welcome"); // It shows message and waits for the user to press ok
+
+// Prompt
+
+let userName = prompt("What is your name", " "); // asking user to input text
+
+// Confirm
+
+let question = confirm("In javascript we have eight data types"); // true or false
