91d2045b520877f186315a754713c69bfd9471b3
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2023-11-14 11:43:31 +0400

    update

--- JavaScript.info/Asynchronous_JS/script.js
+++ JavaScript.info/Asynchronous_JS/script.js
@@ -1,19 +1,71 @@
-const fetchPromise = fetch(
-  "htt://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+/*
+// Callback
+const userLeft = false;
+const userWatchingCatMeme = true;
+
+function watchTutorialCallback(callback, errorCallback) {
+  if (userLeft) {
+    errorCallback({
+      name: "user left",
+      message: ":(",
+    });
+  } else if (userWatchingCatMeme) {
+    errorCallback({
+      name: "User watching cat meme",
+      message: "web dev sip",
+    });
+  } else {
+    callback("Thumbs Up and subscribe");
+  }
+}
+
+watchTutorialCallback(
+  (message) => {
+    console.log(`success: ${message}`);
+  },
+  (error) => {
+    console.log(`failed: ${error.name} ${error.message}`);
+  }
 );
 
-console.log(fetchPromise);
+// Promise
 
-fetchPromise
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
+const p = function () {
+  return new Promise((resolve, reject) => {
+    const userLeft = true;
+    const userWatchingCatMeme = false;
+
+    if (!userLeft && !userWatchingCatMeme) {
+      reject("success");
+    } else {
+      resolve("failed");
     }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data[0].name);
-  })
-  .catch((error) => {
-    console.error(`Could not get products: ${error}`);
   });
+};
+
+p()
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((message) => {
+    console.log(message);
+  });
+*/
+
+const recordVideoOne = new Promise((resolve, reject) => {
+  resolve("Video 1 Recorded");
+});
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+  let x = 4;
+  resolve("Video 2 Recorded");
+});
+const recordVideoThree = new Promise((resolve, reject) => {
+  resolve("Video 3 Recorded");
+});
+
+Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
+  (message) => {
+    console.log(message);
+  }
+);
